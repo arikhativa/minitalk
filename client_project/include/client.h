ba3e60f9ed636d90373570aa325bc95d48de5d47
Yoav Rabby <yrabby@c2r7s3.42madrid.com>
2022-08-06 15:49:35 +0200

    server and client can send reciv signals

--- client_project/include/client.h
+++ client_project/include/client.h
@@ -6,14 +6,27 @@
 /*   By: yrabby <yrabby@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/06 14:31:00 by yrabby            #+#    #+#             */
-/*   Updated: 2022/08/06 14:59:35 by yrabby           ###   ########.fr       */
+/*   Updated: 2022/08/06 15:48:01 by yrabby           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CLIENT_H
 # define CLIENT_H
 
+# include <unistd.h>
+# include <signal.h>
+
 # include "input.h"
+# include "ft_printf.h"
 # include "libminitalk.h"
+
+typedef	struct s_client_meta
+{
+	int	server_pid;
+}				t_client_meta;
+
+
+void	client_signal_init(int pid);
+t_error_code	send_str(t_client_meta *m, char *s);
 
 #endif
--- server_project/include/server.h
+++ server_project/include/server.h
@@ -6,13 +6,20 @@
 /*   By: yrabby <yrabby@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/06 14:23:44 by yrabby            #+#    #+#             */
-/*   Updated: 2022/08/06 14:28:38 by yrabby           ###   ########.fr       */
+/*   Updated: 2022/08/06 15:34:36 by yrabby           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SERVER_H
 # define SERVER_H
 
-# include "pid_print.h"
+# include <signal.h>
+# include <unistd.h>
+
+# include "define.h"
+# include "ft_printf.h"
+
+void	server_handler(int sig, siginfo_t *info, void *context);
+void	server_signal_init(int pid);
 
 #endif
